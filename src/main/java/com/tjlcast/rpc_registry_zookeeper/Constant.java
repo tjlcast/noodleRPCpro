8500a403d26379d1280454585e029bbb07502644
tangjialiang <phx108@sina.com>
2018-08-21 11:10:48 +0800

    new data.

--- src/main/java/com/tjlcast/rpc_registry_zookeeper/Constant.java
+++ src/main/java/com/tjlcast/rpc_registry_zookeeper/Constant.java
@@ -2,6 +2,14 @@ package com.tjlcast.rpc_registry_zookeeper;
 
 /**
  * Created by tangjialiang on 2018/5/7.
+ *
+ * zk 的文件系统:
+ *  /registry/
+ *          + service1//address-111
+ *                     /address-112
+ *          + service2/address-111
+ *                    /address-113
+ *          + service3/...
  */
 public interface Constant {
     String ZK_REGISTRY_PATH = "/registry" ;
